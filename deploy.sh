cf04659771836d6799e5e68656c9a7ef2de8d3e8
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-07-06 16:39:51 +0200

    Cleanup

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-yarn build
-cd build
-git init
-git add .
-git commit -m 'Deploy to Github Pages'
-git push -f git@github.com:oleksanderkorn/joystream-live.git main:gh-pages
-cd ..
